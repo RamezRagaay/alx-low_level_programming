7e984ef87c38b0eb90972c20a189290258502d27
ramezragaay <ramez5ragay@gmail.com>
2023-02-17 19:11:36 -0800

    Hello

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * 
+ * Return: always 0
+ */
+int main()
+{
+
+    char c;
+
+    for (c = 'a' ; c <= 'z' ; c++)
+    {
+        putchar(c);
+    }
+    for (c = 'A' ; c <= 'Z' ; c++)
+    {
+        putchar(c);
+    }
+    return 0;
+}
