99defa5bfd29b2955511d3a926b4502e404da975
ramezragaay <ramez5ragay@gmail.com>
2023-02-27 15:29:02 -0800

    Hello pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - function that change the value of a variable inside memmory
+ *
+ * @*n: parameter
+ *
+ * Return: always 0;
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
