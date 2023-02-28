d1424d7944ad6f84ac50b5f37538cee38fdac591
ramezragaay <ramez5ragay@gmail.com>
2023-02-27 16:33:09 -0800

    Hello

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+<<<<<<< HEAD
+=======
+#include "main.h"
+>>>>>>> 9eca9861949412c61675f202b42affa9a405a2c1
+/**
+ * main - this is the main function
+ *
+ * Return: aalways 0
+ */
+int main(void)
+{
+<<<<<<< HEAD
+	printf("_putchar\n");
+=======
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+>>>>>>> 9eca9861949412c61675f202b42affa9a405a2c1
+	return (0);
+}
