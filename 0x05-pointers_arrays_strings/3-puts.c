9e658dc9a054fd98ac84fdd29127318957857946
ramezragaay <ramez5ragay@gmail.com>
2023-02-27 16:36:57 -0800

    Hello put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
+ *_puts - function that prints
  *
- *
- *
+ *@str: a string to be printed
  * 
+ * Return: always 0;
  */
 
 void _puts(char *str)
 {
 	int count;
+
 	for (count = 0; str[count] != '\0'; count++)
 		_putchar (str[count]);
+	_putchar ('\n');
 }
