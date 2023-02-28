b928ae0ad118890f57a7770fea95f01b772c4f5b
ramezragaay <ramez5ragay@gmail.com>
2023-02-27 17:18:26 -0800

    REVERSE

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int count;
+	int i;
+
+	for (count = 0; s[count] != '\0'; count++)
+		continue;
+	for (i = 0; i < (count / 2); i++)
+	{
+		s[i] = s[count - i];
+	}
+	for (count = 0; s[count] != '\0'; count++)
+		_putchar(s[count];
